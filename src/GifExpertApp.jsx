982082d5a6ba255f69f131388847da3e4b598fb7
MartinVillegas25 <martin@MacBook-Air-de-Martin.local>
2023-04-14 15:40:21 -0300

    Input creado

--- src/GifExpertApp.jsx
+++ src/GifExpertApp.jsx
@@ -1,10 +1,21 @@
 
 import { useState } from 'react';
+import { AddCategory } from './components/AddCategory';
+
 
 
 export const GifExpertApp = () => {
 
-    const [categories, setCatefories] = useState(['One Punch', 'Dragon Ball']);
+    const [categories, setCategories] = useState(['One Punch', 'Dragon Ball']);
+
+    const ondAddCategory = () => {
+        setCategories([...categories, 'Valorant']);v /// el operador spread (...) agarra todo lo que ya tenemos y le agrega lo que le sigue a la coma. 
+
+
+        /// para insertarlo al inicio seria: ['valorant', ...categories]
+    }
+
+
 
 
 
@@ -16,8 +27,12 @@ export const GifExpertApp = () => {
         
             {/* Input */}
 
+            <AddCategory/>
 
             {/* Listado de Gif */}
+            <button onClick={ondAddCategory}>Agregar </button>
+
+
                 {/* Gif Item */}
 
             <ol>
